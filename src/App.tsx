484b7682944c6fe19c61cdebfc38de7a0b261519
SobanAhmed95 <167895255+SobanAhmed95@users.noreply.github.com>
2024-05-16 17:21:24 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [txt , settxt] = useState('abc');
+  const [txtlist , setxtlist] = useState<any>([]);
+  const del = (i:any) => {
+    txtlist.splice(i, 1);
+    setxtlist([...txtlist]);
+
+  }
+  const edi = (index : number) => { 
+    const newtxt = prompt("Enter your new index")
+    if(newtxt !== null){
+      const newlist = [...txtlist];
+      newlist[index] = newtxt;
+     setxtlist(newlist);
+
+    }
+    
+  } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="App">
+        <h4>{txt}</h4>
+        <input type="text" id='inp' onChange={(e) => {
+          settxt(e.target.value);
+          console.log(e.target.value);
+        }}/>
+        <button className='add' onClick={() => {
+             txtlist.push(txt);
+             setxtlist([...txtlist]);
+        }}>Add</button>
+        <button className='del' onClick={(i:any) => {
+             txtlist.splice(i);
+             setxtlist([...txtlist]);
+             
+        }}>Delete All</button>
+        {txtlist.map((x:any , i:any) => (
+          <p className='p' key={i}><span className='sp'>{x}</span> 
+          <p>
+          <button onClick={() => del(i)} className='del'>Delete</button>
+          <button onClick={() => edi(i)} className='edi'>Edit</button>
+          </p>
+          </p>
+        ))}
+    </body>
   );
 }
 
